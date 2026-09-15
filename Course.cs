7a2b1b9c34806c12935617a65fc343498ae55c15
idasamssongmailcom <idasamsson@gmail.com>
2026-09-15 15:26:13 +0200

    Fält och konstruktor i Course

---
+++ Course.cs
@@ -0,0 +1,15 @@
+class Course
+{
+    public string Name;
+
+    public int MaxSeats;
+
+    public List<Course> Students = [];
+
+    public Course(string name, int maxSeats)
+    {
+        string Name = name;
+        int MaxSeats = maxSeats;
+    }
+}
+
